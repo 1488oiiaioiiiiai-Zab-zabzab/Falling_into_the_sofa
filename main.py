456a17299958f232203d00312b08efa9c1883fc0
Dota_tinker <vs1914sochi@gmail.com>
2025-01-11 14:20:39 +0300

    добавил выход из игры кнопкой ESC

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 if __name__ == '__main__':
     pygame.init()
@@ -19,10 +20,22 @@ if __name__ == '__main__':
     text_surface = font.render(button_text, True, (255, 255, 255))
     text_rect = text_surface.get_rect(center=(width // 2, height // 2))
 
+
+    def check_keyboard_input():
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            pygame.quit()
+            sys.exit()
+
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                a = pygame.key.get_pressed()
+                if a[pygame.K_ESCAPE]:
+                    running = False
         screen.fill((0, 0, 0))
         screen.blit(background_image, (0, 0))
         screen.blit(text_surface, text_rect)

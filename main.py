ab2672ad4445acc40be01819ca641c22f1c88c2f
Dota_tinker <vs1914sochi@gmail.com>
2025-01-16 15:39:14 +0300

    добавил функции для загрузки карты, добавил первый тестовый файл с картой, сделал тайл с камнями и добавил картинку главного героя

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import pygame
 import sys
 import sqlite3
+import os
 
 FPS = 60
-
-pygame.init()
-info = pygame.display.Info()
-width, height = info.current_w, info.current_h
-
-screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
-pygame.display.set_caption("game")
 
 
 def getalldatafromsaveslot(slotnumber):
@@ -114,15 +108,15 @@ def saveslots():
                 elif button3.collidepoint(event.pos):
                     buttonpressed = 3
                 elif play_button1.collidepoint(event.pos) and buttonpressed == 1:
-                    print("Играть нажато")
+                    return
                 elif delete_button1.collidepoint(event.pos) and buttonpressed == 1:
                     print("Удалить сохранение нажато")
                 elif play_button2.collidepoint(event.pos) and buttonpressed == 2:
-                    print("Играть нажато")
+                    return
                 elif delete_button2.collidepoint(event.pos) and buttonpressed == 2:
                     print("Удалить сохранение нажато")
                 elif play_button3.collidepoint(event.pos) and buttonpressed == 3:
-                    print("Играть нажато")
+                    return
                 elif delete_button3.collidepoint(event.pos) and buttonpressed == 3:
                     print("Удалить сохранение нажато")
                 else:
@@ -190,11 +184,99 @@ def saveslots():
         pygame.display.flip()
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_level(filename):
+    filename = "maps/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image1 = tile_images[tile_type]
+        self.image = pygame.transform.scale(self.image1, (tile_width, tile_height))
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                pass
+            elif level[y][x] == '#':
+                Tile('floor', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
 def terminate():
     pygame.quit()
     sys.exit()
 
 
 if __name__ == '__main__':
+    pygame.init()
+    info = pygame.display.Info()
+    width, height = info.current_w, info.current_h
+    screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
+    pygame.display.set_caption("game")
+
+    all_sprites = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+
     start_screen()
     saveslots()
+
+    tile_width = tile_height = (height + width) // 50
+
+    tile_images = {'floor': load_image("tiles/floor.png")}
+
+    player_image = load_image('player/maincharacter.png')
+
+    player, level_x, level_y = generate_level(load_level('map1.txt'))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                a = pygame.key.get_pressed()
+                if a[pygame.K_ESCAPE]:
+                    terminate()
+        screen.fill(pygame.Color('black'))
+        tiles_group.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
